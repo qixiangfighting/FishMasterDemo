8c9abd231384208676706e00a21c0e3451a1ec55
Summer <summer.qi@ef.com>
2020-07-28 11:27:43 +0800

    What is layer?

---
+++ FishMasterDemo/Assets/Scripts/Effect/Ef_WaterWave.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ef_WaterWave : MonoBehaviour
+{
+    public Texture[] texttures;
+
+    private Material material;
+
+    private int index = 0;
+
+
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+        InvokeRepeating("ChangeTextture", 0, 0.1f);
+    }
+
+    void ChangeTextture()
+    {
+        material.mainTexture = texttures[index];
+        index = (index + 1) % texttures.Length;
+    }
+}
