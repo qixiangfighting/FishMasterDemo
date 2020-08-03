2fd252735b6337f4c1a3b39129d431783a8b5620
Summer <summer.qi@ef.com>
2020-08-03 13:33:25 +0800

    Add gun script

---
+++ FishMasterDemo/Assets/Scripts/GunFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFollow : MonoBehaviour
+{
+    public RectTransform UGUICanva;
+    public Camera mainCamera;
+    void Update()
+    {
+        Vector3 mousePos;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(UGUICanva,
+            new Vector2(Input.mousePosition.x, Input.mousePosition.y), mainCamera, out mousePos);
+
+        float z;
+
+        if (mousePos.x > transform.position.x)
+        {
+            z = -Vector3.Angle(Vector3.up, mousePos - transform.position);
+        }
+        else
+        {
+            z = Vector3.Angle(Vector3.up, mousePos - transform.position);
+        }
+
+        transform.localRotation = Quaternion.Euler(0,0,z);
+    }
+}
