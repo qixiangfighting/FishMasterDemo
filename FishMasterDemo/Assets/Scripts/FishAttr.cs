58c326b4319f11fea59c66e733c00f6c6c07d783
Summer <summer.qi@ef.com>
2020-08-04 17:56:14 +0800

    Fish in border

--- FishMasterDemo/Assets/Scripts/FishAttr.cs
+++ FishMasterDemo/Assets/Scripts/FishAttr.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,4 +7,12 @@ public class FishAttr : MonoBehaviour
 {
     public int maxNum;
     public int maxSpeed;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Border")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
