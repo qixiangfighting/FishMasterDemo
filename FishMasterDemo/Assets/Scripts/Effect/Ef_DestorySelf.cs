71870f44c8b9dde622fbe4a1b3d85185e581e31a
Summer <summer.qi@ef.com>
2020-08-05 13:42:58 +0800

    Web damage fish

---
+++ FishMasterDemo/Assets/Scripts/Effect/Ef_DestorySelf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ef_DestorySelf : MonoBehaviour
+{
+
+
+    public float delay = 1f;
+
+    private void Start()
+    {
+        Destroy(gameObject, delay);
+    }
+}
--- FishMasterDemo/Assets/Scripts/FishAttr.cs
+++ FishMasterDemo/Assets/Scripts/FishAttr.cs
@@ -8,6 +8,9 @@ public class FishAttr : MonoBehaviour
     public int maxNum;
     public int maxSpeed;
 
+    public GameObject diePrefab;
+    public int hp;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Border")
@@ -18,7 +21,15 @@ public class FishAttr : MonoBehaviour
 
     void TakeDamage(int value)
     {
+        hp -= value;
 
-
+        if (hp <= 0)
+        {
+            GameObject die = Instantiate(diePrefab);
+            die.transform.SetParent(gameObject.transform.parent, false);
+            die.transform.position = transform.position;
+            die.transform.rotation = transform.rotation;
+            Destroy(gameObject);
+        }
     }
 }
