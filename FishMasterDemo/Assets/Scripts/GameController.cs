ed7d4bb76d2c0a743ec687f07655aeaacf982971
Summer <summer.qi@ef.com>
2020-08-05 10:41:03 +0800

    Switch gun

--- FishMasterDemo/Assets/Scripts/GameController.cs
+++ FishMasterDemo/Assets/Scripts/GameController.cs
@@ -1,30 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public GameObject[] gunGos;
 
+    public Text oneShootCostText;
+
     // 使用的是第几挡炮弹
     private int costIndex = 0;
+
     // 没炮所需的金币数 和 造成的伤害值
     private int[] oneShootCosts =
         {5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
 
+
+    private void Update()
+    {
+        ChangeBulletCost();
+    }
+
     public void OnButtonPDown()
     {
-        gunGos[costIndex/4].SetActive(false);
+        gunGos[costIndex / 4].SetActive(false);
         costIndex++;
         costIndex = (costIndex > oneShootCosts.Length - 1) ? 0 : costIndex;
-        gunGos[costIndex/4].SetActive(true);
+        gunGos[costIndex / 4].SetActive(true);
+        oneShootCostText.text = "$" + oneShootCosts[costIndex];
     }
 
     public void OnButtonMDown()
     {
-        gunGos[costIndex/4].SetActive(false);
+        gunGos[costIndex / 4].SetActive(false);
         costIndex--;
-        costIndex = (costIndex < 0 ) ? oneShootCosts.Length - 1 : costIndex;
-        gunGos[costIndex/4].SetActive(true);
+        costIndex = (costIndex < 0) ? oneShootCosts.Length - 1 : costIndex;
+        gunGos[costIndex / 4].SetActive(true);
+        oneShootCostText.text = "$" + oneShootCosts[costIndex];
+    }
+
+    void ChangeBulletCost()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            OnButtonMDown();
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            OnButtonPDown();
+        }
     }
 }
