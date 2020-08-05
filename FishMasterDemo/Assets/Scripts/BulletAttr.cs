80d46756d8cb4b9d6c92e16fd99714eb913a72c5
Summer <summer.qi@ef.com>
2020-08-05 12:17:45 +0800

    web generate

--- FishMasterDemo/Assets/Scripts/BulletAttr.cs
+++ FishMasterDemo/Assets/Scripts/BulletAttr.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,7 +9,23 @@ public class BulletAttr : MonoBehaviour
     public int speed;
     public int damage;
 
+    public GameObject webPrefab;
 
 
+    private void OnTriggerEnter2D(Collider2D collosion)
+    {
+        if (collosion.tag == "Border")
+        {
+            Destroy(gameObject);
+        }
 
+        if (collosion.tag == "Fish")
+        {
+
+            GameObject web = Instantiate(webPrefab);
+            web.transform.SetParent(gameObject.transform.parent, false);
+            web.transform.position = gameObject.transform.position;
+            Destroy(gameObject);
+        }
+    }
 }
